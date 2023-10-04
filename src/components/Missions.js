affa0d687c1d27ee44b1755b0339629e7d06f576
Lucy Wanjiru Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-10-04 16:01:28 +0300

    Revert "Uploading files in the feature/fetch-and-display-mission"

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,44 +1,7 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  fetchMissions,
-  joinMission,
-  leaveMission,
-} from '../redux/missions/missionsSlice';
+import React from 'react';
 import '../styles/Missions.css';
 
 function Missions() {
-  const missionData = useSelector((store) => store.missions);
-  const {
-    isLoading, missions, error, joinedMissions,
-  } = missionData;
-  const dispatch = useDispatch();
-
-  function handleJoinMission(id) {
-    dispatch(joinMission(id));
-  }
-
-  function handleLeaveMission(id) {
-    dispatch(leaveMission(id));
-  }
-
-  useEffect(() => {
-    dispatch(fetchMissions());
-  }, [dispatch]);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return (
-      <div>
-        Error:
-        {error}
-      </div>
-    );
-  }
-
   return (
     <>
       <table className="missions">
@@ -51,42 +14,21 @@ function Missions() {
           </tr>
         </thead>
         <tbody>
-          {missions.map((mission) => (
+          {((mission) => (
             <tr
               key={mission.id}
-              className={
-                joinedMissions.includes(mission.id) ? 'rows' : 'join-rows'
-              }
             >
               <td className="td-mission">{mission.name}</td>
               <td className="description td-description">
                 {mission.description}
               </td>
               <td className="status">
-                {joinedMissions.includes(mission.id) ? (
-                  <span className="mission-status-leave">Active Member</span>
-                ) : (
-                  <span className="mission-status-join">NOT A MEMBER</span>
-                )}
+                <span className="mission-status-leave">Active Member</span>
+                <span className="mission-status-join">Active Member</span>
               </td>
               <td className="button">
-                {joinedMissions.includes(mission.id) ? (
-                  <button
-                    type="button"
-                    className="td-leave-mission"
-                    onClick={() => handleLeaveMission(mission.id)}
-                  >
-                    Leave Mission
-                  </button>
-                ) : (
-                  <button
-                    type="button"
-                    className="td-join-mission"
-                    onClick={() => handleJoinMission(mission.id)}
-                  >
-                    Join Mission
-                  </button>
-                )}
+                <button type="button" className="td-leave-mission">Leave Mission</button>
+                <button type="button" className="td-join-mission">Join Mission</button>
               </td>
             </tr>
           ))}
