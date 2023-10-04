93f896b8b466a9070609295309c25b641c5c0cf1
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-10-04 12:56:45 +0300

    Create a simple structure for the missions page

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,3 +1,35 @@
-const Missions = () => <h2>Missions page</h2>;
+import React from 'react';
+import '../styles/Missions.css';
+
+function Missions() {
+  return (
+    <>
+      <table className="missions">
+        <thead>
+          <tr>
+            <th className="mission">Mission</th>
+            <th className="description">Description</th>
+            <th className="status">Status</th>
+            <th className="button">&nbsp;</th>
+          </tr>
+        </thead>
+        <tbody>
+          {((mission) => (
+            <tr
+              key={mission.id}
+            >
+              <td className="td-mission">{mission.name}</td>
+              <td className="description td-description">
+                {mission.description}
+              </td>
+              <td className="status" />
+              <td className="button" />
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
 
 export default Missions;
