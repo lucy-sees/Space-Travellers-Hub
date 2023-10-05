8dbd0e2794d59f7973d57424909e158c65ad06de
siddghosh108 <siddhartha@anixsoft.co.in>
2023-10-05 17:39:44 +0530

    Created the Missions.test.js file and changed the Navigation.test.js file

--- src/tests/Navigation.test.js
+++ src/tests/Navigation.test.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'; // Import jest-dom to extend expect
+import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Navigation from '../components/Navigation';
 
-test('renders the logo and navigation links', () => {
-  render(
+test('renders Navbar correctly', () => {
+  const { asFragment } = render(
     <MemoryRouter>
       <Navigation />
     </MemoryRouter>,
   );
 
-  // Verify that the logo is present - using alt text
-  const logo = screen.getByAltText('logo');
-  expect(logo).toBeInTheDocument();
-
-  // Verify that the Missions link is present with the text "Missions"
-  const missionsLink = screen.getByRole('link', { name: /Missions/i });
-  expect(missionsLink).toBeInTheDocument();
-
-  // Verify that the Rockets link is present with the text "Rockets"
-  const rocketsLink = screen.getByRole('link', { name: /Rockets/i });
-  expect(rocketsLink).toBeInTheDocument();
+  expect(asFragment()).toMatchSnapshot();
 });
