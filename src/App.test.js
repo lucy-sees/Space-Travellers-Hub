9cb8fbf0793be26145801658d2b365c75e069f64
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-10-05 16:04:53 +0300

    Update test file

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders entire page', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Space Travelers' Hub/i);
-  expect(linkElement).toBeInTheDocument();
-});
