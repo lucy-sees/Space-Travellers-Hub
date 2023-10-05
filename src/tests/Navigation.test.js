1eed0b5af2a4e03bacc2610294fe72824195a532
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-10-05 14:46:47 +0300

    Update Navigation.test.js

--- src/tests/Navigation.test.js
+++ src/tests/Navigation.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; // Import jest-dom to extend expect
 import { MemoryRouter } from 'react-router-dom';
 import Navigation from '../components/Navigation';
 
@@ -10,15 +11,15 @@ test('renders the logo and navigation links', () => {
     </MemoryRouter>,
   );
 
-  // Verify that the logo is present
-  const logo = screen.getByRole('img', { name: /logo/i });
+  // Verify that the logo is present - using alt text
+  const logo = screen.getByAltText('logo');
   expect(logo).toBeInTheDocument();
 
-  // Verify that the missons links are present
+  // Verify that the Missions link is present with the text "Missions"
   const missionsLink = screen.getByRole('link', { name: /Missions/i });
   expect(missionsLink).toBeInTheDocument();
 
-  // Verify that the rockets links are present
+  // Verify that the Rockets link is present with the text "Rockets"
   const rocketsLink = screen.getByRole('link', { name: /Rockets/i });
   expect(rocketsLink).toBeInTheDocument();
 });
